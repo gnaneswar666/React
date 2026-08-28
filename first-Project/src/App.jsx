e89722f4c6a7fc57525b593e877cb3ae7e9f5df8
gnaneswar666 <gnaneswarreddy666@gmail.com>
2026-08-28 14:38:57 +0530

    all previous files added

---
+++ first-Project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from "./components/Home.jsx"
+import About from "./components/About.jsx"
+const App = () => {
+  return (
+    <div>
+
+<Home/>
+<About/>
+    </div>
+  )
+}
+
+export default App
